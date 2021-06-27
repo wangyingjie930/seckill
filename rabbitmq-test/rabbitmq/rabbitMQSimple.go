8c715051c1375dfe627ffcce798a3623efa0befc
wangyingjie <930055912@qq.com>
2021-06-27 18:21:59 +0800

    feat rabbitmq simple模式 test

--- rabbitmq-test/rabbitmq/rabbitMQSimple.go
+++ rabbitmq-test/rabbitmq/rabbitMQSimple.go
@@ -16,9 +16,9 @@ func (r *RabbitMQ) PublishSimple(message string) {
 		r.Queue,
 		//是否持久化
 		false,
-		//是否自动删除
+		//自动删除的前提是: 至少有一个消费者连接到这个队列，之后所有与这个队列连接的消费者都断开时，才会 自动删除
 		false,
-		//是否具有排他性, 其他账号不可访问
+		//如果一个队列被声明为排 他队列，该队列仅对首次声明它的连接可见，并在连接断开时自动删除。
 		false,
 		//是否阻塞处理
 		false,
@@ -30,8 +30,8 @@ func (r *RabbitMQ) PublishSimple(message string) {
 	err = r.channel.Publish(
 		r.Exchange,     // exchange
 		r.Queue, // routing key
-		false,  // mandatory
-		false,  // immediate
+		false,  // 如果为true，根据自身exchange类型和routekey规则无法找到符合条件的队列会把消息返还给发送者
+		false,  // 如果为true，当exchange发送消息到队列后发现队列上没有消费者，则会把消息返还给发送者
 		amqp.Publishing{
 			ContentType: "text/plain",
 			Body:        []byte(message),
@@ -53,10 +53,15 @@ func (r *RabbitMQ) ConsumeSimple()  {
 
 	msgs, err := r.channel.Consume(
 		r.Queue, // queue
+		//用来区分多个消费者
 		"",     // consumer
-		true,  // auto-ack
+		//是否自动应答
+		true,   // auto-ack
+		//是否独有
 		false,  // exclusive
+		//设置为true，表示 不能将同一个Conenction中生产者发送的消息传递给这个Connection中 的消费者
 		false,  // no-local
+		//列是否阻塞
 		false,  // no-wait
 		nil,    // args
 	)
