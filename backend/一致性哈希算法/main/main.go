bc9b023a173e2786a58b04eceb707fb8b3ef39ef
wangyingjie <930055912@qq.com>
2021-07-18 18:01:12 +0800

    feat: binarySearch && consistent Hash

---
+++ backend/一致性哈希算法/main/main.go
@@ -0,0 +1,17 @@
+package main
+
+import (
+	"fmt"
+	consistentTest "imooc-product/backend/一致性哈希算法"
+)
+
+func main() {
+	ips := []string{"192.168.1.1", "192.168.1.2", "192.168.1.3", "192.168.2.3", "192.168.2.4"}
+	consist := consistentTest.NewConsistent()
+	for _, v := range ips {
+		consist.Add(v)
+	}
+	fmt.Printf("%v\n", consist)
+	fmt.Println(consist.Get("hello"))
+	fmt.Println(consist.Get("1qwe1"))
+}
