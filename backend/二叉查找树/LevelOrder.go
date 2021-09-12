769aa15f73ee118f5afefcebde2cd469abd630fb
wangyingjie <930055912@qq.com>
2021-09-12 20:18:27 +0800

    feat: 有效括号

--- backend/二叉查找树/LevelOrder.go
+++ backend/二叉查找树/LevelOrder.go
@@ -1,5 +1,21 @@
 package BST
 
+/**
+给你一个二叉树，请你返回其按 层序遍历 得到的节点值。 （即逐层地，从左到右访问所有节点）。
+
+    3
+   / \
+  9  20
+    /  \
+   15   7
+
+
+[
+  [3],
+  [9,20],
+  [15,7]
+]
+ */
 func LevelOrder(root *Node) [][]int {
 	if root == nil {
 		return [][]int{}
@@ -27,6 +43,12 @@ func LevelOrder(root *Node) [][]int {
 
 /**
 给定一个二叉树，返回其节点值的锯齿形层序遍历。（即先从左往右，再从右往左进行下一层遍历，以此类推，层与层之间交替进行）。
+
+[
+  [3],
+  [20,9],
+  [15,7]
+]
  */
 func ZigzagLevelOrder(root *Node) [][]int {
 	if root == nil {
